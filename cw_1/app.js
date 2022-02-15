d0e702a4eb98ea5fd642461272e45d69bc78be97
Poprotska <marta1404lviv@gmail.com>
2022-02-15 21:59:07 +0200

    cw1

--- cw_1/app.js
+++ cw_1/app.js
@@ -1,9 +1,85 @@
 const fs = require('fs');
 const path = require('path')
 
+//Task 1
 
-fs.mkdir(path.join(__dirname, 'cw_1', 'text'), (err) => {
+fs.mkdir(path.join(__dirname, 'TASK-1'), (err) =>{
     if (err) {
-        console.log(err)
+        console.log(err);
     }
 })
+
+fs.writeFile(path.join(__dirname, 'TASK-1', 'TASK-1.txt'),  'TASK WORK !!!!!!!!', (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
+
+
+fs.readFile(path.join(__dirname, 'TASK-1', 'TASK-1.txt'), 'utf8',(err, data ) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log(data)
+})
+
+
+fs.mkdir(path.join(__dirname, 'TEXT'), (err) =>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+//
+fs.rename(path.join(__dirname,  'TASK-1', 'TASK-1.txt'),
+    path.join(__dirname, 'TEXT', 'TASK-1.txt'), (err ) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+
+// TASK-2
+
+fs.mkdir(path.join(__dirname, 'TASK-2'), (err) =>{
+    if (err) {
+        console.log(err);
+    }
+})
+
+fs.writeFile(path.join(__dirname, 'TASK-2', 'TASK-2.txt'),  'name:PETRO, \nage:19, \ncity:Rivne', (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
+
+fs.readFile(path.join(__dirname, 'TASK-2', 'TASK-2.txt'), 'utf8',(err, data ) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log(data)
+})
+
+
+fs.mkdir(path.join(__dirname, 'TEXT-2'), (err) =>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
+
+//
+fs.rename(path.join(__dirname,  'TASK-2', 'TASK-2.txt'),
+    path.join(__dirname, 'TEXT-2', 'TASK-2.txt'), (err ) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+
+fs.unlink(path.join(__dirname, 'TASK-2'), (err)=> {
+        if (err) {
+          console.log(err);
+ }
+});
